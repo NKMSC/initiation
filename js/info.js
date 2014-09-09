5d60b5b414a06b2aea7d821a8f930ce0ad047710
New Future <new_future@msn.cn>
2014-09-09 20:44:47 +0800

    解决中js获取参数文乱码的问题

--- js/info.js
+++ js/info.js
@@ -2,8 +2,9 @@ email=document.getElementsByTagName('email');
 for (var i = 0;	i < email.length;	i++) {
 	email[i].innerHTML=email[i].innerHTML.replace('AT', '&#64;').replace('DOT', '&#46;');
 };
-
 var p = location.search.substr(1);
-if(p){
-	document.getElementById("info").innerHTML="<span>"+p+"</span>";
+if(p){msg=decodeURI(p);
+	if(msg){
+		document.getElementById("info").innerHTML="<span>"+msg+"</span>";
+	}
 }

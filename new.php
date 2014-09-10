6f8ec2503565883bee4041046264e129c3099af5
New Future <new_future@msn.cn>
2014-09-10 11:22:26 +0800

    邮件主体修改

--- new.php
+++ new.php
@@ -18,15 +18,18 @@ function GetPost($name)
 function Error($error_info)
 {
     //die($error_info);
-    Header("Location: http://q.nkumstc.cn/error.html?$error_info");
+	Header("Location: http://q.nkumstc.cn/error.html?$error_info");
 }
 
 //跳转到成功页面
-function Success($suc_info)
+function Success($suc_info,$table)
 {
-    
-    //echo $suc_info;
-    Header("Location: http://q.nkumstc.cn/success.html?$suc_info"); 
+	$head='<head><style type="text/css">table{border-collapse:collapse;}table,td, th{border:1px solid black;padding:5px;}</style></head>';
+	$body='<h3>'.$suc_info.'</h3>'.$table."<h4>如果信息有误请前于报名入口<a href='http://q.nkumstc.cn'>q.kumstc.cn</a>再次录入信息！</h4><b>需要更多信息你可以访问<a href='http://nkumstc.cn'>南微软官方主页</a>,或者在下面的媒体渠道关注联系我们</b>人人：个人主页<a href=\"http://www.renren.com/318793631/profile\">南微软</a><br>人人：公共主页<a href=\"http://page.renren.com/601898669\">南微软</a><br>微博：<a href=\"http://weibo.com/nkumstc\">南开大学微软技术俱乐部</a><br><small>为了确保您能及时收到邮件，请将此邮箱添加入通讯录或者白名单，避免邮件有可能会进入垃圾箱！</small><small>如果以上不是你的个人信息请忽略此邮件！</small></body>";
+	$msg='<html>$head $body</html>';
+	global $email;
+	SendEmail($email,$msg);
+	Header("Location: http://q.nkumstc.cn/success.html?$suc_info,请留意你的邮箱$email"); 
 }
 
 $gender_list = array('F','M');
@@ -103,16 +106,9 @@ if($check_result&&mysql_fetch_array($check_result))
 	if($result)
 	{
         //发送邮件
-$msg="<html><head><style type=\"text/css\">table{border-collapse:collapse;}table, td, th{border:1px solid black;padding:5px;}</style></head><body><h3>报名信息修改成功</h3><table id=\"table\"><thead><tr><td>学号</td><td>姓名</td><td>性别</td><td>学院</td><td>年级</td><td>手机</td><td>邮箱</td><td>意愿一</td><td>意愿二</td></tr></thead><tbody><tr><td>$id</td><td>$name</td><td>$RealGender</td><td>$college</td><td>$grade</td><td>$phone</td><td>$email</td><td>$dept1</td><td>$dept2</td></tr><tr><td>备注</td><td colspan=\"8\">$info</td></tr></tbody></table>人人：个人主页<a href=\"http://www.renren.com/318793631/profile\">南微软</a><br>人人：公共主页<a href=\"http://www.renren.com/318793631/profile\">南微软</a><br>微博：<a href=\"http://weibo.com/nkumstc\">南开大学微软技术俱乐部</a><br><small>为了确保您能及时收到邮件，请将我们添加为好友，否则邮件会进入垃圾箱</small></body></html>";
+		$table="<table id=\"table\"><thead><tr><td>学号</td><td>姓名</td><td>性别</td><td>学院</td><td>年级</td><td>手机</td><td>邮箱</td><td>意愿一</td><td>意愿二</td></tr></thead><tbody><tr><td>$id</td><td>$name</td><td>$RealGender</td><td>$college</td><td>$grade</td><td>$phone</td><td>$email</td><td>$dept1</td><td>$dept2</td></tr><tr><td>备注</td><td colspan=\"8\">$info</td></tr></tbody></table>";
+		Success('修改信息成功',$table);
 
-            $SendEmailoResult=SendEmail($email,$msg);
-            if($SendEmailoResult==true){
-                Success('修改信息成功');
-            }else{
-                Error("发送邮件失败"+$SendEmailoResult);
-            }
-        
-        
 	}else{
 		//更新失败
 		Error("更新数据失败:".mysql_error());
@@ -122,19 +118,10 @@ $msg="<html><head><style type=\"text/css\">table{border-collapse:collapse;}table
 	$result=mysql_query($strSQL);//成功返回资源标识符，失败返回false
 	if($result)
 	{
-$msg="<html><head><style type=\"text/css\">table{border-collapse:collapse;}table, td, th{border:1px solid black;padding:5px;}</style></head><body><h3>报名信息提交成功</h3><table id=\"table\"><thead><tr><td>学号</td><td>姓名</td><td>性别</td><td>学院</td><td>年级</td><td>手机</td><td>邮箱</td><td>意愿一</td><td>意愿二</td></tr></thead><tbody><tr><td>$id</td><td>$name</td><td>$RealGender</td><td>$college</td><td>$grade</td><td>$phone</td><td>$email</td><td>$dept1</td><td>$dept2</td></tr><tr><td>备注</td><td colspan=\"8\">$info</td></tr></tbody></table>人人：个人主页<a href=\"http://www.renren.com/318793631/profile\">南微软</a><br>人人：公共主页<a href=\"http://www.renren.com/318793631/profile\">南微软</a><br>微博：<a href=\"http://weibo.com/nkumstc\">南开大学微软技术俱乐部</a><br><small>为了确保您能及时收到邮件，请将我们添加为好友，否则邮件会进入垃圾箱</small></body></html>";
-
-            $SendEmailoResult=SendEmail($email,$msg);
-            if($SendEmailoResult==true){
-                Success('提交成功');
-            }else{
-                Error("发送邮件失败"+$SendEmailoResult);
-            }
-        
-        
-		
+		$table="<table id=\"table\"><thead><tr><td>学号</td><td>姓名</td><td>性别</td><td>学院</td><td>年级</td><td>手机</td><td>邮箱</td><td>意愿一</td><td>意愿二</td></tr></thead><tbody><tr><td>$id</td><td>$name</td><td>$RealGender</td><td>$college</td><td>$grade</td><td>$phone</td><td>$email</td><td>$dept1</td><td>$dept2</td></tr><tr><td>备注</td><td colspan=\"8\">$info</td></tr></tbody></table>";
+		Success('报名信息提交成功',$table);		
 	}else{
-        Error("插入数据失败:".mysql_error());
+		Error("插入数据失败:".mysql_error());
 	}
 }
 ?>

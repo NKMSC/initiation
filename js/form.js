3ab48cd4973d09e896dfd42fdc32986647afb2d2
New Future <new_future@msn.cn>
2014-09-16 12:19:57 +0800

    加强学号验证，研究生自动填�

--- js/form.js
+++ js/form.js
@@ -72,150 +72,192 @@ function select(s,v){
 	}
 }
 
-function checkid( id ){   
-	var r = /^[0-9]{4,}[x|x]?$/; 
-	if( r.test(id))
+function checkid( id ){  
+	var r ; 
+	var auto=arguments[1]?arguments[1]:true;
+	var b;
+	var g=document.getElementById('grade');
+	switch (id.length)
 	{
-		var auto=arguments[1]?arguments[1]:true;
-		if(auto){
-			var g=document.getElementById('grade');
-			switch(id.substr(0,2)){
-				case '11'://11级大四
-				select(g,'大四');
-				break;
-
-				case '12'://大三
-				select(g,'大三');
-				break;
-				case '13':select(g,'大二');
-				break;
-				case '14':select(g,'大一');
-				break;
+		case 7:
+		r = /^[1][0-4]\d{5}$/;
+		if( !r.test(id))return false;		
+		if(auto)
+		{				
+			switch(id.substr(0,2))
+			{
+					case '11'://11级大四
+					select(g,'大四');
+					break;
+					case '12'://大三
+					select(g,'大三');
+					break;
+					case '13':select(g,'大二');
+					break;
+					case '14':select(g,'大一');
+					break;
+					case '10':select(g,'研一');
+					break;
+				}
 			}
-		}
-		rmv_err('id');
-		return true;
-	}else{
-		err('id','请填写正确的学号！(学号我们仅作为核实身份用^_^)');
-		return false;
-	}
-}
-function checkname( n ){
-	var r = /^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]){2,10}$/;   
-	if(r.test(n)){
-		rmv_err('name');
-		return true;
-	}else{
-		err('name',"请输填写你的中文姓名！");
-		return false;
-	}
-}
-function checkgender(){
-
-	var R=document.getElementsByName('gender');
-	for (var i =R.length - 1; i >=0; i--) {
-		if(R[i].checked)
-		{
-			rmv_err('gender');
+			rmv_err('id');
 			return true;
+			break;
+
+			case 10:
+			r=/^\d{2}[1-2][0|9][0|1|8|9]\d{5}$/;
+			if( !r.test(id)) return false;		
+			if(auto)
+			{				
+				switch(id.substr(2,4))
+				{
+					case '2014':
+					select(g,'研一');
+					break;
+					case '2013':
+					select(g,'研二');
+					break;
+					case '2012':select(g,'研三');
+					break;
+				}
+			}
+			rmv_err('id');
+			return true;
+
+			break;
+			case 15:
+			r=/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/;break;
+			case 18:
+			r=/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;
+			break;
+			default:
+			err('id','请填写正确的学号！(学号我们仅作为核实身份用^_^)');
+			return false;
 		}
-	};
-	err('gender','性别二选一!')
-	return false;
-}
+		if( r.test(id))
+		{
+			rmv_err('id');
+			return true;
+		}else{
+			return false;
+		}
+	}
+	function checkname( n ){
+		var r = /^([\u4E00-\uFA29]|[\uE7C7-\uE7F3]){2,10}$/;   
+		if(r.test(n)){
+			rmv_err('name');
+			return true;
+		}else{
+			err('name',"请输填写你的中文姓名！");
+			return false;
+		}
+	}
+	function checkgender(){
 
-function checkcollege(c){
-	if(isNull(c))
-	{
-		err('college','请选择学院,如果列表中没有你所在的学院请选择“其他”，并在备注中说明！');
-		return false;
-	}else{
-		rmv_err('college');
-		return true;
-	}
-}
-
-function checkgrade(g){
-	if(isNull(g))
-	{
-		err('grade','请选择你本学期所在的年级,如果你不是南开大学的学生，请选择“其他”并在备注中说明！');
-		return false;
-	}else{
-		rmv_err('grade');
-		return true;
-	}
-}
-function checkphone(p){
-	var r =/^1[3458]{1}\d{9}$/; 
-	if(r.test(p))
-	{
-		rmv_err('phone');
-		return true;
-	}else{
-		err('phone','请填写,你的国内11位手机号!(方便我们短信通知你^_^)');
-		return false;
-	}
-}
-function checkemail(e) { 
-	var r= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
-	if(r.test(e)){
-		rmv_err('email');
-		return true;
-	}else{
-		err('email',"请填写正确的邮箱！(详细信息我们会发送邮件给你^_^)");
-		return false;
-	}
-}
-function checkdept1(d){
-	if(isNull(d))
-	{
-		err('dept1','请选择你希望加入的部门！(技术部会有笔试(^▽^))');
-		return false;
-	}else{
-		var d2=document.getElementById('dept2');
-		var no=d2.options[0];
-		no.disabled=false;
-		no.text="不选择";
-		no.value="无";
-		for (var i = d2.options.length - 1; i >= 1; i--) {
-			var o=d2.options[i];
-			if(o.value==d){
-				o.disabled=true;
-				d2.style.color=IC;
-			}else{
-				o.disabled=false;
-				o.selected=true;
+		var R=document.getElementsByName('gender');
+		for (var i =R.length - 1; i >=0; i--) {
+			if(R[i].checked)
+			{
+				rmv_err('gender');
+				return true;
 			}
 		};
-		rmv_err('dept1');
-		return true;
+		err('gender','性别二选一!')
+		return false;
 	}
-}
-function checkform(){
-	var isok=true;
-	
-	var id=V('id');
-	isok&=checkid(id,false);
-	
-	var name=V('name');
-	isok&=checkname(name);
 
-	isok&=checkgender();
+	function checkcollege(c){
+		if(isNull(c))
+		{
+			err('college','请选择学院,如果列表中没有你所在的学院请选择“其他”，并在备注中说明！');
+			return false;
+		}else{
+			rmv_err('college');
+			return true;
+		}
+	}
 
-	var college=V('college');
-	isok&=checkcollege(college);
+	function checkgrade(g){
+		if(isNull(g))
+		{
+			err('grade','请选择你本学期所在的年级,如果你不是南开大学的学生，请选择“其他”并在备注中说明！');
+			return false;
+		}else{
+			rmv_err('grade');
+			return true;
+		}
+	}
+	function checkphone(p){
+		var r =/^1[3458]{1}\d{9}$/; 
+		if(r.test(p))
+		{
+			rmv_err('phone');
+			return true;
+		}else{
+			err('phone','请填写,你的国内11位手机号!(方便我们短信通知你^_^)');
+			return false;
+		}
+	}
+	function checkemail(e) { 
+		var r= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
+		if(r.test(e)){
+			rmv_err('email');
+			return true;
+		}else{
+			err('email',"请填写正确的邮箱！(详细信息我们会发送邮件给你^_^)");
+			return false;
+		}
+	}
+	function checkdept1(d){
+		if(isNull(d))
+		{
+			err('dept1','请选择你希望加入的部门！(技术部会有笔试(^▽^))');
+			return false;
+		}else{
+			var d2=document.getElementById('dept2');
+			var no=d2.options[0];
+			no.disabled=false;
+			no.text="不选择";
+			no.value="无";
+			for (var i = d2.options.length - 1; i >= 1; i--) {
+				var o=d2.options[i];
+				if(o.value==d){
+					o.disabled=true;
+					d2.style.color=IC;
+				}else{
+					o.disabled=false;
+					o.selected=true;
+				}
+			};
+			rmv_err('dept1');
+			return true;
+		}
+	}
+	function checkform(){
+		var isok=true;
 
-	var grade=V('grade');
-	isok&=checkgrade(grade);
+		var id=V('id');
+		isok&=checkid(id,false);
 
-	var phone=V('phone');
-	isok&=checkphone(phone);
+		var name=V('name');
+		isok&=checkname(name);
 
-	var email=V('email');
-	isok&=checkemail(email);
+		isok&=checkgender();
 
-	var dept1=V('dept1');
-	isok&=checkdept1(dept1);
+		var college=V('college');
+		isok&=checkcollege(college);
 
-	return Boolean(isok);
-};
+		var grade=V('grade');
+		isok&=checkgrade(grade);
+
+		var phone=V('phone');
+		isok&=checkphone(phone);
+
+		var email=V('email');
+		isok&=checkemail(email);
+
+		var dept1=V('dept1');
+		isok&=checkdept1(dept1);
+
+		return Boolean(isok);
+	};

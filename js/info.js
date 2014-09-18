9df5b749832fb6d22535379a807b3ea4858c6720
New Future <new_future@msn.cn>
2014-09-18 20:57:05 +0800

    代码压缩

--- js/info.js
+++ js/info.js
@@ -1,10 +1,1 @@
-email=document.getElementsByTagName('email');
-for (var i = 0;	i < email.length;	i++) {
-	email[i].innerHTML=email[i].innerHTML.replace('AT', '&#64;').replace('DOT', '&#46;');
-};
-var p = location.search.substr(1);
-if(p){msg=decodeURI(p);
-	if(msg){
-		document.getElementById("info").innerHTML="<span>"+msg+"</span>";
-	}
-}
+email=document.getElementsByTagName("email");for(var i=0;i<email.length;i++){email[i].innerHTML=email[i].innerHTML.replace("AT","@").replace("DOT",".")}var p=location.search.substr(1);if(p){msg=decodeURI(p);if(msg){document.getElementById("info").innerHTML="<span>"+msg+"</span>"}};
